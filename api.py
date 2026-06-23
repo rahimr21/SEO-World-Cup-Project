831eb1091ef2e7f53fe747ecede374269dc2c71c
Rahim Rashid <rahimrashid06@gmail.com>
2026-06-23 18:53:11 +0000

    Added fetch teams function

--- api.py
+++ api.py
@@ -14,3 +14,29 @@ FOOTBALL_URL = "https://v3.football.api-sports.io"
 LEAGUE_ID = 1
 SEASON = 2026
 
+def get_football_headers():
+    return {
+        "x-apisports-key": FOOTBALL_KEY
+    }
+
+def fetch_teams():
+    url = f"{FOOTBALL_URL}/teams"
+    params = {
+        "league": LEAGUE_ID,
+        "season": SEASON
+    }
+
+    response = requests.get(url, headers=get_football_headers(), params=params)
+    data = response.json()
+
+    teams_list = []
+    for item in data.get("response", []):
+        team_data = item.get("team", {})
+        teams_list.append({
+            "id": team_data.get("id"),
+            "name": team_data.get("name"),
+            "code": team_data.get("code")
+        })
+    return teams_list
+
+print(fetch_teams())
