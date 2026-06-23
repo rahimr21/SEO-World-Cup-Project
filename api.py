c0be6f7ca5721bcc18bc38d27c77ef320657d0dd
Anika Hakim <hakimanika3529@gmail.com>
2026-06-23 18:29:53 +0000

    news functions in api

--- api.py
+++ api.py
@@ -4,3 +4,24 @@ from dotenv import load_dotenv
 
 FOOTBALL_KEY = os.getenv("FOOTBALL_KEY")
 NEWS_KEY = os.getenv("NEWS_KEY")
+
+def get_news(team):
+    params = {
+        "q": f"{team} AND World Cup 2026",
+        "sortBy": "publishedAt",
+        "apiKey": NEWS_KEY
+    }
+    r = requests.get("https://newsapi.org/v2/everything", params=params)
+    articles = r.json().get("articles", [])
+    return articles[:5]
+
+def get_current_news():
+    params = {
+        "q": "World Cup 2026",
+        "sortBy": "publishedAt",
+        "apiKey": NEWS_KEY
+    }
+    r = requests.get("https://newsapi.org/v2/everything", params=params)
+    articles = r.json().get("articles", [])
+    return articles[:5]
+    
