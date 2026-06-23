6e562337f5a43dc72bd683af7cc8e92ecd7f123c
Anika Hakim <hakimanika3529@gmail.com>
2026-06-23 17:36:02 +0000

    test

---
+++ main.py
@@ -0,0 +1,7 @@
+def home_menu():
+    print("\nWorld Cup")
+    print("1. Team Information")
+    print("2. Player Information")
+    print("3. Current Games")
+    print("4. View Favorites")
+    choice = input("Enter the number which you want to select: ")
