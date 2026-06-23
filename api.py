e8e0d68bb0fd62617eae265c54a862124813cf9f
Rahim Rashid <rahimrashid06@gmail.com>
2026-06-23 18:39:17 +0000

    adding requirements.txt

--- api.py
+++ api.py
@@ -2,5 +2,15 @@ import os
 import requests
 from dotenv import load_dotenv
 
+load_dotenv()
+
 FOOTBALL_KEY = os.getenv("FOOTBALL_KEY")
 NEWS_KEY = os.getenv("NEWS_KEY")
+
+#Base URLs
+FOOTBALL_URL = "https://v3.football.api-sports.io"
+
+#ID's for API to fetch 2026 World Cup Stuff
+LEAGUE_ID = 1
+SEASON = 2026
+
